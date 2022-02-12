691658c4e6da85b44de824efacb3f615008a192c
GitHub Action <noreply@github.com>
2022-02-12 16:04:58 +0000

    Update sub-web from sunyou726/sub-web

--- sub-web/js/precache-manifest.1ee5f371d42459b3269bd6e9f4607481.js
+++ sub-web/js/precache-manifest.1ee5f371d42459b3269bd6e9f4607481.js
@@ -4,7 +4,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "_redirects"
   },
   {
-    "revision": "8d267e50c515dd8e06f3",
+    "revision": "866f08484487f7d11363",
     "url": "css/app.6a5dab7f.css"
   },
   {
@@ -20,16 +20,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "fonts/element-icons.732389de.ttf"
   },
   {
-    "revision": "ede0d2455a94c3bb9fe6aa6b94ce8921",
+    "revision": "0c16daef745c8d9a012a07c0d6f02ca1",
     "url": "index.html"
   },
   {
-    "revision": "8d267e50c515dd8e06f3",
-    "url": "js/app.6a46f033.js"
+    "revision": "866f08484487f7d11363",
+    "url": "js/app.9da2f339.js"
   },
   {
-    "revision": "8d4add1601c839d374fe",
-    "url": "js/chunk-87bd93be.75466138.js"
+    "revision": "65a928b78038289f28d1",
+    "url": "js/chunk-87bd93be.1d1da842.js"
   },
   {
     "revision": "b013401d6352bbde8b80",
